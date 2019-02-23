ed50f1b68bedd1952ef4f6bdb2e405102a7d41e2
Vladislav Arbatov <mail@vladislav.tv>
2019-02-24 02:54:36 +0300

    Separated static site on gh-pages

--- site/_run_dev.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-#workon neuronaming
-FLASK_APP=server.py flask run --host=0.0.0.0 --port=5000
--- torch-rnn/init.lua
+++
@@ -1,6 +0,0 @@
-require 'torch'
-require 'nn'
-
-require 'torch-rnn.LSTM'
-require 'torch-rnn.VanillaRNN'
-require 'torch-rnn.TemporalCrossEntropyCriterion'
